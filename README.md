9820db565efd53466266cab387abaef056584419
naranjito72 <theclick36@gmail.com>
2021-05-19 19:53:54 +0200

    starting finally and setting up

--- README.md
+++ README.md
@@ -4,7 +4,7 @@ This project was generated with [Angular CLI](https://github.com/angular/angular
 
 ## Development server
 
-Run `ng serve` for a dev server. Navigate to `http://localhost:4200/`. The app will automatically reload if you change any of the source files.
+Run `npm start`  or `ng serve` for a dev server. Navigate to `http://localhost:4500/`. The app will automatically reload if you change any of the source files.
 
 ## Code scaffolding
 
@@ -16,12 +16,18 @@ Run `ng build` to build the project. The build artifacts will be stored in the `
 
 ## Running unit tests
 
-Run `ng test` to execute the unit tests via [Karma](https://karma-runner.github.io).
-
-## Running end-to-end tests
-
-Run `ng e2e` to execute the end-to-end tests via [Protractor](http://www.protractortest.org/).
+No running test in this app.
 
 ## Further help
 
 To get more help on the Angular CLI use `ng help` or go check out the [Angular CLI Overview and Command Reference](https://angular.io/cli) page.
+
+
+## Documentation
+
+  * Fixing [**fsevents**](https://stackoverflow.com/questions/46929196/how-to-solve-npm-install-throwing-fsevents-warning-on-non-mac-os) ✔
+
+  * [**@popperjs/core**](https://popper.js.org/docs/v2/) V2 ✔
+
+  * Bootstrap version [**5.0**](https://getbootstrap.com/docs/5.0/getting-started/download/) ✔
+  
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,8 +1,28 @@
 import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { LOCALE_ID, NgModule } from '@angular/core';
+
+// location
+import localeESP from '@angular/common/locales/es';
+import { registerLocaleData } from '@angular/common';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+
+// comunicating with back-end
+import { HttpClient, HttpClientModule } from '@angular/common/http';
+
+// popUps UI/UX answers
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { ToastrModule } from 'ngx-toastr';
+
+// reactive forms if needed
+import { ReactiveFormsModule } from '@angular/forms';
+
+
+// where are we
+registerLocaleData(localeESP);
+
+
 
 @NgModule({
   declarations: [
@@ -10,9 +30,26 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClient,
+    HttpClientModule,
+    // ngx-toastr
+    BrowserAnimationsModule,
+    ToastrModule.forRoot({
+      preventDuplicates: true
+    }),
+    // reactive forms
+    ReactiveFormsModule, // !important, do not remove
+
+
   ],
-  providers: [],
+  providers: [
+    {
+      provide: LOCALE_ID,
+      useValue: 'es-ES'
+    }
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
+
