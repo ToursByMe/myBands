e7aac57224c8f91fa8279a2601658ad73c954fcf
naranjito72 <theclick36@gmail.com>
2021-05-20 14:04:05 +0200

    is not bootstrap 4 anymore

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,8 @@
 import { Component } from '@angular/core';
 
+// bootstrap js import statement
+import 'bootstrap/js/dist/collapse'; // ~navbar, ~transitions, ~accordion
+import 'bootstrap/js/dist/carousel';
 @Component({
   selector: 'bc-root',
   templateUrl: './app.component.html',
