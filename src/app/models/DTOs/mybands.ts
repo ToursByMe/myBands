599ea4738775828d14f736fb75b7cd5c406833ce
naranjito72 <theclick36@gmail.com>
2021-05-24 11:49:19 +0200

    making json server

--- src/app/models/DTOs/mybands.ts
+++ src/app/models/DTOs/mybands.ts
@@ -1,35 +1,64 @@
-import { Albums } from "./albums";
 import { Members } from "./members";
 
 export class Mybands {
+  private id: string;
   private name: string;
-  private arrMembers: Members[] = [];
-  private arrAlbums: Albums[] = [];
+  private arrMembers: string[] = [];
+  private album: string;
+  private yearAlbum: string;
+  private photoAlbum: string;
 
-  constructor(name: string) {
-    this.name    = name;
+
+  constructor(id: string, name: string, member: string[], album: string, year: string, photo: string) {
+    this.id         = id;
+    this.name       = name;
+    this.album      = album;
+    this.arrMembers = member;
+    this.yearAlbum  = year;
+    this.photoAlbum = photo;
 
   }
 
   public get myName(){
     return this.name;
   }
+   public get myId(){
+     return this.id;
+   }
+
+   public get myAlbum(){
+     return this.album;
+   }
+   public get myYearAlbum() {
+     return this.yearAlbum;
+   }
+   public get myPhotoAlbum () {
+     return this.photoAlbum;
+   }
 
   public set myName(name: string) {
     this.name = name;
   }
+public  set myId(id: string) {
+  this.id = id;
+}
+public set myAlbum(album: string){
+   this.album = album;
+}
+public set myYearAlbum( year) {
+   this.yearAlbum = year;
+}
+public set myPhotoAlbum (photo) {
+   this.photoAlbum = photo;
+}
 
 // methods
-public addArrMembers(member: Members) {
+public addArrMembers(member: string) {
   this.arrMembers.push(member)
 }
 
-public addArrAlbums(album: Albums) {
-  this.arrAlbums.push(album);
-}
-
 public showArrays(arr: any[]) {
-let showMe: any = arr.map((e: any) => e.name);
+let showMe: any = arr.map((e: any) => e);
 return showMe;
 }
 
